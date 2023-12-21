a17c9be8d529c071e474f6ef773b78125f965007
unknown <damiankrak@gmail.com>
2023-12-21 12:17:04 +0100

    prealloc to aria2.conf

--- downloader/run/run_aria2.sh
+++ downloader/run/run_aria2.sh
@@ -7,5 +7,5 @@ touch /log/aria2_log.txt
 tor '--runasdaemon' 1
 
 exec v2ray run -c /conf/config.json &
-exec aria2c --conf-path=/conf/aria2.conf --log=/log/aria2_log.txt --rpc-listen-port=${RPCPORT} --rpc-secret=${RPCSECRET} --file-allocation=prealloc
+exec aria2c --conf-path=/conf/aria2.conf --log=/log/aria2_log.txt --rpc-listen-port=${RPCPORT} --rpc-secret=${RPCSECRET}
 
