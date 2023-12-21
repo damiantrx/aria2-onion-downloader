6979e60bc5ad324b700f83e68dc8be0ddc088b50
unknown <damiankrak@gmail.com>
2023-12-21 12:01:18 +0100

    CMD with bash, aria2c with --file-allocation=prealloc

--- downloader/run/run_aria2.sh
+++ downloader/run/run_aria2.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 touch /conf/aria2.session
 touch /log/aria2_log.txt
 
-tor --runasdaemon 1
+tor '--runasdaemon' 1
 
 exec v2ray run -c /conf/config.json &
-exec aria2c --conf-path=/conf/aria2.conf --log=/log/aria2_log.txt --rpc-listen-port=${RPCPORT} --rpc-secret=${RPCSECRET}
+exec aria2c --conf-path=/conf/aria2.conf --log=/log/aria2_log.txt --rpc-listen-port=${RPCPORT} --rpc-secret=${RPCSECRET} --file-allocation=prealloc
 
